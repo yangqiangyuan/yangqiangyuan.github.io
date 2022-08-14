9bc6579be10c19840440f4d8de21b6de1b923bcd
yangqiangyuan <1106439330@qq.com>
2022-08-14 10:33:08 +0800

    Site updated: 2022-08-14 10:33:08

--- js/main.js
+++ js/main.js
@@ -1,182 +1,204 @@
-var app = new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data(){
+require([], function (){
+
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
+        })
+    }
+    var isPCInit = false;
+    var loadPC = function(){
+        require([yiliaConfig.rootUrl + 'js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
+        })
+    }
+
+    var browser = {
+        versions: function() {
+        var u = window.navigator.userAgent;
         return {
-            currentPage: 1,
-            nightMode: "wb_sunny",
-            menuHover: false,
-            tagHover: false,
-            commentTab: 0,
-            commentConfig: null,
-            commentFunction: {       
-                gitalk: ()=>{
-                    var gitalk = new Gitalk({
-                        clientID: this.commentConfig.gitalk_client_id,
-                        clientSecret: this.commentConfig.gitalk_client_secret,
-                        repo: this.commentConfig.gitalk_repo,
-                        owner: this.commentConfig.gitalk_owner,
-                        admin: [this.commentConfig.gitalk_owner],
-                        id:  md5(location.pathname) ,
-                        distractionFreeMode: this.commentConfig.gitalk_distractionFreeMode,
-                    });
-                    gitalk.render('gitalk-container');
-                },
-                valine: ()=>{
-                    var option = {
-                        el: '#vcomments',
-                        appId: this.commentConfig.valine_leancloud_app_id,
-                        appKey: this.commentConfig.valine_leancloud_app_key,
-                    };
-                    new Valine(Object.assign(option, this.commentConfig.valine_option));
-                },
-                changyan: ()=>{
-                    (function(){
-                        var appid = this.commentConfig.changyan_app_id;
-                        var conf = this.commentConfig.changyan_app_key;
-                        var width = window.innerWidth || document.documentElement.clientWidth;
-                        if (width < 960) {
-                            window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); 
-                        } 
-                    })(); 
-                },
-                disqus: ()=>{
-                    (function () {
-                        var d = document, s = d.createElement('script');
-                        s.src = '//'+this.commentConfig.disqus_shortname+'.disqus.com/embed.js';
-                        s.setAttribute('data-timestamp', +new Date());
-                        (d.head || d.body).appendChild(s);
-                    })();
-                },
-                livere: ()=>{
-                    (function (d, s) {
-                        var j, e = d.getElementsByTagName(s)[0];
-                        if (typeof LivereTower === 'function') { return; }
-                        j = d.createElement(s);
-                        j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-                        j.async = true;
-                        e.parentNode.insertBefore(j, e);
-                    })(document, 'script');
-                },
-            },
-            searchHeaderValue: null,
-            searchPageValue: null,
-            searchData: new Array(),
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
+    }
+
+    $(window).bind("resize", function() {
+        if (isMobileInit && isPCInit) {
+            $(window).unbind("resize");
+            return;
         }
-    },
+        var w = $(window).width();
+        if (w >= 700) {
+            loadPC();
+        } else {
+            loadMobile();
+        }
+    });
 
-    methods:{
-        PageChange: function(){
-            if(this.currentPage==1){
-                window.location.href = location.pathname.split("page/")[0];
-            }else{
-                window.location.href = location.pathname.split("page/")[0] +'page/' + this.currentPage + "/";
-            };
-        },
-        SetNightMode: function(){
-            if (this.$vuetify.theme.dark == true){
-                this.$vuetify.theme.dark = false;
-                localStorage.removeItem('insulin-dark');
-                this.nightMode = "wb_sunny";
-            }else{
-                this.$vuetify.theme.dark = true;
-                localStorage.setItem('insulin-dark', true);
-                this.nightMode = "brightness_2";
-            }
-        },
-        EnterSearch: function(varSearch,isLocal){
-            if(isLocal){
-                window.location.href = "/search/?" + encodeURI(varSearch); 
-            } else {
-                window.location.href = "https://www.google.com/search?q=" + encodeURI(varSearch) + " site:" + window.location.hostname;
-            };
-        },   
-        Search: function(varStr){
-            keywords=varStr.split(/\s+/);
-            var str="";
-            for (const data of this.searchData) {
-                var data_title = data.title;
-                var data_url = data.url;
-                var data_content = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase();
-                var index_content = -1;
-                if (data_content !== '') {
-                for (const keyword of keywords) {
-                    if(index_content==-1){
-                    index_content = data_content.indexOf(keyword);
-                    }
-                };
-                };
-                if(index_content!=-1){
-                var start = index_content - 20;
-                var end = index_content + 80;
-                if(start < 0){start = 0;};
-                if(end > data_content.length){end = data_content.length;};
-                str += "<div class='search-post'><a class='search-post-title' href='" + data_url + "'>" + data_title + "</a><p class='search-post-content'>" + data_content.substr(start,end) + "</p><hr></div>";
-                };
-            };
-            for (const keyword of keywords) {
-                str=str.replace(eval('/'+keyword+'/g'),"<span class='search-post-bold'>" + keyword + "</span>")
-            };
-            if(str.length==0){
-                str="<div class='search-post'>暂无</div>";
-            };
-            document.getElementById("search-result").innerHTML=str;
-        },
-    },
+    if(!!browser.versions.mobile || $(window).width() < 800){
+        loadMobile();
+    } else {
+        loadPC();
+    }
 
-    mounted: function () {
-        //初始化currentPage
-        ((varpage)=>{
-            if(varpage){
-                this.currentPage = parseInt(varpage);
-            };
-        })(location.pathname.split("page/")[1]);
-        //初始化NightMode
-        ((isDark)=>{
-            if(isDark){
-                this.$vuetify.theme.dark = true;
-                this.nightMode = "brightness_2";
-            }
-        })(localStorage.getItem('insulin-dark'));
-        //搜索
-        ((path)=>{
-            if(path=='/search/'){
-                axios({
-                    methods:"GET",
-                    url: "/search.xml",
-                    responseType: 'document',
-                }).then(res=>{
-                    var xmlDoc = res.data.getElementsByTagName("entry");
-                    //初始化搜索
-                    for (const post of xmlDoc) {
-                        this.searchData.push({
-                        title: post.getElementsByTagName("title")[0].childNodes[0].nodeValue,
-                        content: post.getElementsByTagName("content")[0].childNodes[0].nodeValue,
-                        url: post.getElementsByTagName("url")[0].childNodes[0].nodeValue,
-                        });
+    resetTags = function(){
+        var tags = $(".tagcloud a");
+        for(var i = 0; i < tags.length; i++){
+            var num = Math.floor(Math.random()*7);
+            tags.eq(i).addClass("color" + num);
+        }
+        $(".article-category a:nth-child(-n+2)").attr("class", "color0");
+    }
+
+    // fancyBox
+    if(!!yiliaConfig.fancybox){
+        require([yiliaConfig.fancybox_js], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    if(typeof(title) == "undefined"){
+                        var title = imgArr.eq(i).attr("title");
                     }
-                    //搜索
-                    this.searchPageValue=decodeURI(location.search.substr(1));
-                    this.Search(this.searchPageValue); 
-                }).catch(error=>{
-                    console.error(error);
-                });
-            }
-        })(location.pathname);
-        //初始化评论设置
-        ((el)=>{
-            if(el){
-                this.commentConfig = JSON.parse(Base64.decode(el.getAttribute("data")));
-                for (let e in this.commentConfig.use) {
-                    eval('this.commentFunction.' + this.commentConfig.use[e] + '();');
+                    var width = imgArr.eq(i).attr("width");
+                    var height = imgArr.eq(i).attr("height");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' width="+width+" height="+height+" title='"+title+"' alt='"+title+"'></a>");
                 }
+                $(".article-inner .fancy-ctn").fancybox({ type: "image" });
             }
-        })(document.getElementById('tabs-content'));
-        //Google Adsense
-        (()=>{
-            for (let index = 0; index < document.getElementsByClassName('adsense-contain').length; index++) {
-                (adsbygoogle = window.adsbygoogle || []).push({});
+        })
+    }
+
+    // Animate on Homepage
+    if(!!yiliaConfig.animate) {
+        if(!!yiliaConfig.isHome) {
+            require([yiliaConfig.scrollreveal], function (ScrollReveal) {
+                var animationNames = [
+                "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+                ],
+                len = animationNames.length,
+                randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
+                // Fallback (CSS3 keyframe, requestAnimationFrame)
+                if (!window.requestAnimationFrame) {
+                    $('.body-wrap > article').css({opacity: 1});
+                    if (navigator.userAgent.match(/Safari/i)) {
+                        function showArticle(){
+                            $(".article").each(function(){
+                                if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                                    $(this).removeClass("hidden").addClass("show");
+                                    $(this).addClass("is-hiddened");
+                                } else {
+                                    if(!$(this).hasClass("is-hiddened")) {
+                                        $(this).addClass("hidden");
+                                    }
+                                }
+                            })
+                        }
+                        $(window).on('scroll', function(){
+                            showArticle();
+                        });
+                        showArticle();
+                    }
+                    return;
+                }
+
+                var animateScope = ".body-wrap > article";
+                var $firstArticle = $(".body-wrap > article:first-child");
+                if ($firstArticle.height() > $(window).height()) {
+                    var animateScope = ".body-wrap > article:not(:first-child)";
+                    $firstArticle.css({opacity: 1});
+                }
+                ScrollReveal({
+                    duration: 0,
+                    afterReveal: function (domEl) {
+                        $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1})
+                    }
+                }).reveal(animateScope);
+            })
+        } else {
+            $('.body-wrap > article').css({opacity: 1});
+        }
+    }
+
+    // TOC
+    if (yiliaConfig.toc) {
+        require(['toc'], function(){ })
+    }
+
+    // Random Color 边栏顶部随机颜色
+    var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
+    var id = Math.ceil(Math.random()*(colorList.length-1));
+    // PC
+    $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
+    // Mobile
+    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
+
+    // Table
+    $("table").wrap("<div class='table-area'></div>");
+
+    // Hide Comment Button
+    $(document).ready(function() {
+        if ($("#comments").length < 1) {
+            $("#scroll > a:nth-child(2)").hide();
+        }
+    })
+
+    // Hide Labels
+    if(yiliaConfig.isArchive || yiliaConfig.isTag || yiliaConfig.isCategory) {
+        $(document).ready(function() {
+            $("#footer").after("<button class='hide-labels'>TAGS</button>");
+            $(".hide-labels").click(function() {
+                $(".article-info").toggle(200);
+            })
+        })
+    }
+
+    // Task lists in markdown
+    $('ul > li').each(function() {
+        var taskList = {
+            field: this.textContent.substring(0, 2),
+            check: function(str) {
+                var re = new RegExp(str);
+                return this.field.match(re);
             }
-        })();
-    },
+        }
+
+        var string = ["[ ]", ["[x]", "checked"]];
+        var checked = taskList.check(string[1][0]);
+        var unchecked = taskList.check(string[0]);
+
+        var $current = $(this);
+        function update(str, check) {
+            var click = ["disabled", ""];
+            $current.html($current.html().replace(
+              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+            )
+        }
+
+        if (checked || unchecked) {
+            this.classList.add("task-list");
+            if (checked) {
+                update(string[1][0], string[1][1]);
+                this.classList.add("check");
+            } else {
+                update(string[0], "");
+            }
+        }
+    })
+
 })
